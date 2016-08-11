3599dade6a7f5070cd9f8256fc1d437d0c386ae2
EA4GKQ <angel@ayasystems.com>
2016-08-11 15:21:09 +0200

    Create shutdownButton.py

---
+++ shutdownButton.py
@@ -0,0 +1,54 @@
+#Autor Ángel / ayasystems
+#
+# 
+#
+import syslog
+import time
+import RPi.GPIO as GPIO
+import atexit
+
+var = 1
+counter = 0
+global start
+global end
+
+BUTTON_SHUTDOWN = 40#Setup with your GPIO Button
+
+def goodbye():
+    syslog.syslog("Closing script, good bye")
+    GPIO.cleanup()           # clean up GPIO on normal exit
+
+def button_shutdown(channel):
+    global start
+    global end
+    if GPIO.input(BUTTON_SHUTDOWN) == 0:
+        start = time.time()
+#       print"pulsado"
+    if GPIO.input(BUTTON_SHUTDOWN) == 1:
+#        print"libre"
+        end = time.time()
+        elapsed = end - start
+#        print(elapsed)
+        if ( elapsed>2 and elapsed < 5):# entre 2 y 5 segundos
+             syslog.syslog("Raising reboot command")
+             os.system("sudo reboot")
+        if elapsed>=10:# mas de 10 segundos
+             syslog.syslog("Raising shutdown command")
+             os.system("sudo shutdown")
+
+#print("GPIO VERSION: " + str(GPIO.VERSION))
+syslog.syslog("Shutdown button ->  GPIO: " + str(BUTTON_SHUTDOWN))
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(BUTTON_SHUTDOWN,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+atexit.register(goodbye)
+
+GPIO.add_event_detect(BUTTON_SHUTDOWN,GPIO.BOTH,callback=button_shutdown)
+start = time.time()
+end   = time.time()
+
+print(start)
+print(end)
+
+while True:
+
+    time.sleep(1)
