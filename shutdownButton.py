8cf218a783aad00ce33600b12d9584010c1e1c57
EA4GKQ <angel@ayasystems.com>
2017-09-23 12:10:32 +0200

    Update shutdownButton.py

--- shutdownButton.py
+++ shutdownButton.py
@@ -1,4 +1,3 @@
-#Autor Angel / ayasystems
 #
 # 
 #
@@ -7,13 +6,37 @@ import time
 import RPi.GPIO as GPIO
 import atexit
 import os
-
 var = 1
 counter = 0
 global start
 global end
 
-BUTTON_SHUTDOWN = 40#Setup with your GPIO Button
+try:
+    import httplib
+except:
+    import http.client as httplib
+
+
+BUTTON_SHUTDOWN = 40 #Setup with your GPIO Button
+LED_PIN         = 38 
+
+def have_internet():
+#    conn = httplib.HTTPConnection("www.google.com", timeout=5)
+#    try:
+#        conn.request("HEAD", "/")
+#        conn.close()
+#        return True
+#    except:
+#        conn.close()
+#        return False
+	status_eth0  = open('/sys/class/net/eth0/operstate', 'r').read()
+	status_wlan0 = open('/sys/class/net/wlan0/operstate', 'r').read()
+#	print(status_eth0)
+#	print(status_wlan0)
+	if(status_eth0 != 'down' or status_wlan0 != 'down'):
+		return True
+	else:
+		return False
 
 def goodbye():
     syslog.syslog("Closing script, good bye")
@@ -24,32 +47,34 @@ def button_shutdown(channel):
     global end
     if GPIO.input(BUTTON_SHUTDOWN) == 0:
         start = time.time()
-#       print"pulsado"
+        print("pulsado")
     if GPIO.input(BUTTON_SHUTDOWN) == 1:
-#        print"libre"
+        print("libre")
         end = time.time()
         elapsed = end - start
-#        print(elapsed)
-        if ( elapsed>2 and elapsed < 5):# entre 2 y 5 segundos
-             syslog.syslog("Raising reboot command")
-             os.system("sudo reboot now")
-        if elapsed>=10:# mas de 10 segundos
+        print(elapsed)
+        if ( elapsed>0.2:# Pulsacion mayor a 0.2 segudos (evita rebotes/ruido)
              syslog.syslog("Raising shutdown command")
              os.system("sudo shutdown now")
-
-#print("GPIO VERSION: " + str(GPIO.VERSION))
+#             print("shutdown <10")
+print("GPIO VERSION: " + str(GPIO.VERSION))
 syslog.syslog("Shutdown button ->  GPIO: " + str(BUTTON_SHUTDOWN))
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(BUTTON_SHUTDOWN,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(LED_PIN, GPIO.OUT) 
 atexit.register(goodbye)
 
 GPIO.add_event_detect(BUTTON_SHUTDOWN,GPIO.BOTH,callback=button_shutdown)
 start = time.time()
 end   = time.time()
 
-#print(start)
-#print(end)
 
 while True:
 
-    time.sleep(1)
+    time.sleep(0.1)
+    if( have_internet()):
+	GPIO.output(LED_PIN,True)
+	time.sleep(0.1)
+	GPIO.output(LED_PIN,False)
+    else:
+        GPIO.output(LED_PIN,False)
